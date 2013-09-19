6062ebf763a825a7853e24b86b28ca58ed887564
Jaime Forero-Romero <j.e.forero.romero@gmail.com>
2013-09-19 11:54:20 -0500

    updating geometry.c

--- hands_on/make/geometry.c
+++ hands_on/make/geometry.c
@@ -1,15 +1,16 @@
 #include <stdio.h>
+#include <math.h>
 #define PI 3.14159
 
 float get_surface(float radius){
   float sur;
-  sur = 4.0 * PI * radius * radius;
+  sur = 4.0 * PI * pow(radius,2);
   return sur;
 }
 
 float get_volume(float radius){
   float vol;
-  vol = (4.0 / 3.0 ) * PI * radius * radius * radius;
+  vol = (4.0 / 3.0 ) * PI * pow(radius,3);
   return vol;
 }
 
