38763e2831c3ad71dd5e7f1128e0b355e400be53
Jaime Forero-Romero <j.e.forero.romero@gmail.com>
2013-09-19 12:04:26 -0500

    obfuscated c

---
+++ hands_on/skeleton/obfuscated.c
@@ -0,0 +1,6 @@
+int i;main(){for(;i["]<i;++i){--i;}"];read('-'-'-',i+++"hell\
+o, world!\n",'/'/'/'));}read(j,i,p){write(j/p+p,i---j,i/i);}
+/* 
+-- Dishonorable mention, Obfuscated C Code Contest, 1984.
+http://www.doc.ic.ac.uk/lab/cplus/cstyle.html
+*/
