630d1872e700f1b57a4f9a0541236620801d9e55
Jaime Forero-Romero <j.e.forero.romero@gmail.com>
2014-08-20 11:53:33 -0500

    added new file to read

--- hands_on/python/np_loadtxt.py
+++ hands_on/python/np_loadtxt.py
@@ -1,3 +1,8 @@
 import numpy as np
 data = np.loadtxt('lista.txt', dtype={'names':('Name','Age','Grade',),'formats': ('S10', 'i4', 'f4')})
 print data
+
+datos = np.loadtxt('random_xy.dat')
+print datos[:,1]
+print datos[:,0]
+
